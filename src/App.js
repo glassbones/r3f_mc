1073620715889545d731fc38f4bb1c2ff0c82334
Michael <glassbonesofficial@gmail.com>
2021-01-23 15:02:09 -0600

    chest animates on click

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 //styles
 import "./assets/styles/App.scss";
 //three
@@ -5,9 +6,14 @@ import { Canvas } from "react-three-fiber"
 // Model
 import Model from './components/Three/mcChest'
 import Lights from './components/Three/lights'
+import Ground from './components/Three/ground'
 import { Suspense } from "react";
+import { softShadows } from "@react-three/drei";
+
+softShadows()
 
 export default function App() {
+const [isOpen, setIsOpen] = useState(false)
   return (
     <>
       <Canvas
@@ -16,7 +22,8 @@ export default function App() {
         camera={{position: [-5,4,4], fov:40}}>
         <Lights/>
         <Suspense fallback={null}>
-          <Model />
+          <Model isOpen={isOpen} setIsOpen={setIsOpen}/>
+          <Ground/>
         </Suspense>
       </Canvas>
     </>
---
+++ src/components/Three/ground.js
@@ -0,0 +1,7 @@
+import React from "react";
+export default () => 
+<mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]} receiveShadow>
+  <planeBufferGeometry attach='geometry' args={[100, 100]} />
+  <shadowMaterial attach='material' opacity={0.4} />
+</mesh>
+
